146ca85d6cba24a01ea1fa2071502fd814ac0dc0
Michelle-1982 <michellepachecopantoja@gmail.com>
2025-07-05 13:57:16 +0200

    ejercicio-5

--- src/js/main.js
+++ src/js/main.js
@@ -13,3 +13,15 @@ document.querySelector(".list").innerHTML = "<li class='adalaber-M'>Michelle</li
 document.querySelector(".adalab-selection").innerHTML = "La adalaber seleccionada es:";
 document.querySelector(".adalaber-M").style.display = "none"
 const adalabSelection ="La adalaber seleccionada es:";
+
+document.querySelector(".lorem").innerHTML = "<div><h1>Lorem ipsum</h1><img src='http://via.placeholder.com/350x150'/><p>Lorem ipsum</p></div>" 
+
+
+document.querySelector('.myAddress').innerHTML = myAddress;
+const myAddress ="Madrid, 28009";
+let address ="Valladolid, 47009";
+
+
+
+
+
