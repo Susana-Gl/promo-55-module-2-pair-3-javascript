a2ddc06d192070818ee230e6bf84f88adc72f540
Susana.Gl <susiigarcialeon@gmail.com>
2025-07-05 12:59:09 +0200

    5 de julio

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+console.log('>> Ready :)');
+
+document.querySelector(".title").innerHTML = "Esta página no es compatible con la versión actual de tu navegador. Por favor actualízalo a la versión más reciente";
+
+let paragraph = "Hola";
+document.querySelector('.paragraph').innerHTML = paragraph;
+paragraph = "Hola, mundo";
+document.querySelector('.paragraph').innerHTML = paragraph;
+
+document.querySelector(".list").innerHTML = "<li class='adalaber-M'>Michelle</li> <li class='adalaber-S'>Susana</li>"
+document.querySelector(".adalab-selection").innerHTML = "La adalaber seleccionada es:";
+document.querySelector(".adalaber-M").style.display = "none"
+const adalabSelection ="La adalaber seleccionada es:";
