fd219fc441d04befe54eb79a772c1a6d1ef30ed4
Susana.Gl <susiigarcialeon@gmail.com>
2025-07-08 20:40:20 +0200

    Primera parte clase 08 julio

--- src/js/main.js
+++ src/js/main.js
@@ -21,7 +21,28 @@ let myaddress = 'Madrid, 28009';
 myaddress = 'Valladolid, 47009';
 
 
+//
 
+/*const firstName = Maria;
+const secondName = Luisa;
+const firstName = true;
+const secondName = true;
 
+const registered = firstName || secondName;
+console.log ("registered", registered);*/
 
+//
 
+const number = 12;
+
+if (number < 12) {
+  console.log('El número es 0')
+} else if (number < 13) {
+  console.log('El número es negativo')
+} else if (number sum 2) {
+  console.log('El número más 2 es mayor que 13 pero menor o igual que 20')
+} else if (number > 20 && number < 50) {
+  console.log('El número es mayor que 20 pero menor que 50')
+} else {
+  console.log('el número no es 123123125')
+}
